9f7b8f654c019187a7d02289860fb030458137a0
root <root@pisa.localdomain>
2017-10-17 07:42:58 -0200

    dt added

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 
 <?php
-echo "<script type=\"text\/javascript\" src=\"https://js-cdn.dynatrace.com/jstag/147f84b2bdc/ruxitagent_ICA2SVfqr_10129170927131437.js\" data-dtconfig=\"app=280564972A8B1342|cors=1|featureHash=ICA2SVfqr|lastModification=0|dtVersion=10129170927131437|reportUrl=https://bf20190olf.bf.dynatrace.com/bf|tp=500,50,0,1\" crossorigin=\"anonymous\"></script>" ; 
+
+  echo "<script src=\"https://js-cdn.dynatrace.com/jstag/147f84b2bdc/ruxitagent_ICA237SVfgqrtvx_10129170927131437.js\" data-dtconfig=\"app=280564972A8B1342|cors=1|featureHash=ICA237SVfgqrtvx|lastModification=0|dtVersion=10129170927131437|reportUrl=https://bf20190olf.bf.dynatrace.com/bf|tp=500,50,0,1\" crossorigin=\"anonymous\"></script>";
+
   echo "
       <script>
         alert( 'Hello, world!' );
