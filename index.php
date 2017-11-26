8560f6de8737479006610a1ee1eba7c30f2848a2
renatoppuccini <renatoppuccini@gmail.com>
2017-11-26 13:19:49 -0200

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,9 @@
 
 <?php
 
-  echo "<script src=\"https://js-cdn.dynatrace.com/jstag/147f84b2bdc/ruxitagent_ICA237SVfgqrtvx_10129170927131437.js\" data-dtconfig=\"app=280564972A8B1342|cors=1|featureHash=ICA237SVfgqrtvx|lastModification=0|dtVersion=10129170927131437|reportUrl=https://bf20190olf.bf.dynatrace.com/bf|tp=500,50,0,1\" crossorigin=\"anonymous\"></script>";
-
-  echo "
-      <script>
-        alert( 'Hello, world!' );
-      </script>
-  ";
-  echo "<h1>Olá Mundo v4.0</h1>";
-  echo $_SERVER['SERVER_ADDR'];
+echo "<h1>Openshift Workshop</h1> ";
+echo "<h1>Versão Brasileira Alamo/h1> ";
+echo $_SERVER['SERVER_ADDR'];
 
   
 ?>
