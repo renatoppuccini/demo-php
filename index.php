7ce51b916a636383346c7b713cb1d5d6f639034b
renatoppuccini <renatoppuccini@gmail.com>
2017-11-27 13:18:40 -0200

    versao 2

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php
 
 echo "<h1>Openshift Workshop</h1> ";
-echo "<h1>Versão Brasileira Nova ERA </h1> ";
+echo "<h1>Versão Brasileira Herbert Tichards</h1> ";
 echo $_SERVER['SERVER_ADDR'];
 
   
