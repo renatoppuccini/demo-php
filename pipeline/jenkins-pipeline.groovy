2691512e869e2edac28e0d2f35c1934cce2bf943
renatoppuccini <renatoppuccini@gmail.com>
2020-04-01 17:59:18 -0400

    teste script

--- pipeline/jenkins-pipeline.groovy
+++ pipeline/jenkins-pipeline.groovy
@@ -20,22 +20,21 @@ pipeline{
         }
 
         stage('Deploy to DEV'){
-            echo 'Deploying to DEV....'
-            openshiftDeploy(deploymentConfig: 'demo-php')
+            script{
+                openshiftDeploy(deploymentConfig: 'demo-php')
+            }
         }
 
         stage('Promote from DEV to QA'){
-            sleep 5
-            echo 'Promoting to QA...'
+           
         }
 
         stage('Wait for approval'){
-            input 'Aprove to production?'
+           
         }
 
         stage ('Promote from QA to Production'){
-            sleep 3
-            echo 'Promoting to production...'
+            
         }
     }
     
