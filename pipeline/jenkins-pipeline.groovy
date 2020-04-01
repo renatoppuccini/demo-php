1d5fb9f4f2fd84884b3408e47e3cff296dae4189
renatoppuccini <renatoppuccini@gmail.com>
2020-04-01 18:09:13 -0400

    test script

--- pipeline/jenkins-pipeline.groovy
+++ pipeline/jenkins-pipeline.groovy
@@ -4,38 +4,48 @@ pipeline{
             label "php"
         }
     }
-     environment{
+    
+    environment{
         APPLICATION_NAME = 'demo-php'
         DEV_PROJECT = "mydemo-php"
-        BUILDCFG_NAME = "nodejs-webapp"
+        BUILDCFG_NAME = "php-pipeline"
     }
-
+    
     stages{
-        stage('Build image'){
-            steps{
-                script{
-                    openshiftBuild(buildConfig: 'demo-php', showBuildLogs: 'true')
+        stage('Delete buildconfig'){
+            steps {
+                script {
+                    openshift.withCluster() {
+                        openshift.withProject(DEV_PROJECT) {
+                            openshift.selector("all", [ template : BUILDCFG_NAME ]).delete()
+                        }
+                    }
                 }
             }
         }
 
-        stage('Deploy to DEV'){
-            script{
-                openshiftDeploy(deploymentConfig: 'demo-php')
+        stage('Create Image Builder') {
+            when {
+                expression {
+                    openshift.withCluster() {
+                        openshift.withProject(DEV_PROJECT) {
+                            return !openshift.selector("bc", "${BUILDCFG_NAME}").exists();
+                        }
+                    }
+                }
+            }
+           steps {
+                script {
+                    openshift.withCluster( "https://api.ocp4.jwalton.redhat-demo.com:6443") {
+                        openshift.withProject(DEV_PROJECT) {
+                            openshift.newBuild("--name=${BUILDCFG_NAME}", "--image-stream=openshift/nodejs:10", "--binary=true")
+                        }
+                    }
+                }
             }
         }
-
-        stage('Promote from DEV to QA'){
-           
-        }
-
-        stage('Wait for approval'){
-           
-        }
-
-        stage ('Promote from QA to Production'){
-            
-        }
     }
+
+   
     
 }
