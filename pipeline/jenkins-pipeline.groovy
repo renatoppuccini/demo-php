608338ed839efe40ceab20754940092427f0acd6
renatoppuccini <renatoppuccini@gmail.com>
2020-04-01 17:52:46 -0400

    added stages
    stages

--- pipeline/jenkins-pipeline.groovy
+++ pipeline/jenkins-pipeline.groovy
@@ -5,30 +5,31 @@ pipeline{
         }
     }
 
+    stages{
+        stage('Build image'){
+            echo 'Building docker image....'
+            openshiftBuild(buildConfig: 'demo-php', showBuildLogs: 'true')
 
-    stage('Build image'){
-        echo 'Building docker image....'
-        openshiftBuild(buildConfig: 'demo-php', showBuildLogs: 'true')
+        }
 
-    }
+        stage('Deploy to DEV'){
+            echo 'Deploying to DEV....'
+            openshiftDeploy(deploymentConfig: 'demo-php')
+        }
 
-    stage('Deploy to DEV'){
-        echo 'Deploying to DEV....'
-        openshiftDeploy(deploymentConfig: 'demo-php')
-    }
-    
-    stage('Promote from DEV to QA'){
-        sleep 5
-        echo 'Promoting to QA...'
-    }
+        stage('Promote from DEV to QA'){
+            sleep 5
+            echo 'Promoting to QA...'
+        }
 
-    stage('Wait for approval'){
-        input 'Aprove to production?'
-    }
+        stage('Wait for approval'){
+            input 'Aprove to production?'
+        }
 
-    stage ('Promote from QA to Production'){
-        sleep 3
-        echo 'Promoting to production...'
+        stage ('Promote from QA to Production'){
+            sleep 3
+            echo 'Promoting to production...'
+        }
     }
     
 }
