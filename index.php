a368a02e3d35fd03d2406796abd73110efb1abb3
renatoppuccini <renatoppuccini@gmail.com>
2020-06-09 18:57:34 -0400

    blue test

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php
 
 echo "<h1>Openshift Workshop</h1> ";
-echo "<h1 style='color:green;'>Version 1.0  Live webinar!!!! </h1> ";
+echo "<h1 style='color:blue;'>Version 1.0  Live webinar!!!! </h1> ";
 echo $_SERVER['SERVER_ADDR'];
 
 ?>
