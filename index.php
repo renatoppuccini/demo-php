f6af18076c4de695a153888ff5ab809123717402
root <root@cdl-adminrock.lab.wt.us.infinidat.com>
2024-10-01 16:37:24 +0300

    test

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 
 <?php
 
-echo "<h1>Openshift Workshop</h1> ";
+echo "<h1>Openshift Workshop 1</h1> ";
 echo "<h1 style='color:green;'>Version 2.0  Live webinar!!!! </h1> ";
 echo $_SERVER['SERVER_ADDR'];
 
