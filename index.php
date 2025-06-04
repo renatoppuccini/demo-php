bccc5b4bc33ca78202ec8bcc7e11cd7b12ee00d9
renatoppuccini <renatoppuccini@gmail.com>
2025-06-04 15:24:42 -0400

    Update index.php
    blue

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 
 <?php
 
-echo "<h1>Openshift Workshop 1</h1> ";
-echo "<h1 style='color:green;'>Version 2.0  Live webinar!!!! </h1> ";
+echo "<h1>Openshift POC </h1> ";
+echo "<h1 style='color:blue;'>Version 1.0  Live webinar!!!! </h1> ";
 echo $_SERVER['SERVER_ADDR'];
 
 ?>
